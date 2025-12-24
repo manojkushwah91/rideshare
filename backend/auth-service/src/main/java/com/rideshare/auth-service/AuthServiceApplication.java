3a4061e77b78b5c12a29e4f093c9fbde2c0f30e9
Manojkushwah-dev <manojkushwah91115@gmail.com>
2025-12-24 23:56:34 +0530

    some changes

--- backend/auth-service/src/main/java/com/rideshare/auth-service/AuthServiceApplication.java
+++ backend/auth-service/src/main/java/com/rideshare/auth-service/AuthServiceApplication.java
@@ -1,7 +1,7 @@
 package com.rideshare.authservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType; 
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -9,5 +9,6 @@ public class AuthServiceApplication {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(AuthServiceApplication.class);
         app.setWebApplicationType(WebApplicationType.REACTIVE);
+        app.run(args);
     }
 }
